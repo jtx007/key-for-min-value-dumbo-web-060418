05ddb71865aee35748888c17a24843402ddbd94e
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-17 16:42:37 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,9 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   key_for_less = nil
   name_hash.collect do |key , value|
-    if lowest_value > value
+    if lowest_value == nil
+      lowest_value = 0
+    elsif lowest_value > value
       lowest_value = value
       key_for_less = key
     end
