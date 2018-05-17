cc0e07f27b10f056767afe7d6691c2e960d2309e
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-17 16:07:26 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,11 @@
 
 def key_for_min_value(name_hash)
   name_hash.collect do |key , value, key2, value2|
-    if value > value
+    if value2 > value
+      key
+      elsif value > value2
+      key2
+    end
       
 
 end
